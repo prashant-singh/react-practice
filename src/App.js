04ab8fcbfeb91fefbc57c9a3471c7cd1ce64a534
Prashant Singh <prashant19194@gmail.com>
2022-04-23 00:39:24 +0530

    fixed pages.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename='/'>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/calculator' element={<Calculator />} />
           <Route path='/react-resume' element={<ReactResume />} />
